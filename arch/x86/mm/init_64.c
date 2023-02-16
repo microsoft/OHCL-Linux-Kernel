5027e3d81206631f6bf60a1dff5d4b175f68e7a8
Jared White <jaredwhite@microsoft.com>
2023-02-16 20:20:34 +0000

    x86/mm/64: Free the gap between BSS_MAIN and BSS_DECRYPTED.
    This saves up to 2 MiB of memory in the kernel image.
    
    Signed-off-by: Jared White <jaredwhite@microsoft.com>
    (cherry picked from commit 13c2f167f9e23b03093972ae333b07e867c8f13a)

--- arch/x86/mm/init_64.c
+++ arch/x86/mm/init_64.c
@@ -1373,6 +1373,8 @@ void mark_rodata_ro(void)
 	unsigned long end = (unsigned long)__end_rodata_hpage_align;
 	unsigned long text_end = PFN_ALIGN(_etext);
 	unsigned long rodata_end = PFN_ALIGN(__end_rodata);
+	unsigned long bss_decrypted_gap_start = PFN_ALIGN(__start_bss_decrypted_gap);
+	unsigned long bss_decrypted_gap_stop = PFN_ALIGN(__start_bss_decrypted);
 	unsigned long all_end;
 
 	printk(KERN_INFO "Write protecting the kernel read-only data: %luk\n",
@@ -1410,6 +1412,8 @@ void mark_rodata_ro(void)
 				(void *)text_end, (void *)rodata_start);
 	free_kernel_image_pages("unused kernel image (rodata/data gap)",
 				(void *)rodata_end, (void *)_sdata);
+	free_kernel_image_pages("unused kernel image (bss_decrypted gap)",
+				(void *)bss_decrypted_gap_start, (void *)bss_decrypted_gap_stop);
 }
 
 /*
