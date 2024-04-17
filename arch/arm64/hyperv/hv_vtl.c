ffb09a134c7562f3da02a32b703268e7dc1beb18
Sunil Muthuswamy <sunilmut@microsoft.com>
2024-04-16 17:19:57 -0700

    [vtl2] [arm64] BSP doesn't need to enable AP VTL2 anymore
    The BSP had to enable the AP VTL2 earlier because the VTL2 kernel
    was probing the GICR regions before AP boot and the hypervisor
    didn't setup the GICR for AP before the VTL2 was enabled. This
    was causing problems with the VTL2 kernel boot. underhill_boot
    now enables the VP VTL, thereby removing the VTL2 kernel to do
    the same.

--- arch/arm64/hyperv/hv_vtl.c
+++ arch/arm64/hyperv/hv_vtl.c
@@ -12,53 +12,7 @@
 
 static int __init hv_vtl_cpu_init(unsigned int cpu)
 {
-	static struct hv_enable_vp_vtl enable_vp_vtl __aligned(HV_HYP_PAGE_SIZE);
-	u64 status;
-	int ret = 0;
-	/*
-	 * None of the kmalloc et. al. is available at this time and
-	 * neither is hyperv_pcpu_input_arg initialized so far.
-	 */
-	struct hv_enable_vp_vtl *input = &enable_vp_vtl;
-	unsigned long irq_flags;
-	int i;
-
-	/*
-	 * nr_cpu_ids is not set at this time (or essentially, it is just
-	 * NR_CPUS that is known at this time). Loop through and break when
-	 * the hypervisor indicates the VP index is invalid.
-	 */
-	memset(input, 0, sizeof(*input));
-	local_irq_save(irq_flags);
-	for (i = 0; i < nr_cpu_ids; i++) {
-		input->partition_id = HV_PARTITION_ID_SELF;
-		input->vp_index = i;
-		input->target_vtl.target_vtl = HV_VTL_MGMT;
-		input->vp_context.pc = (u64)__pa_symbol(secondary_entry);
-
-		/*
-		 * This has to be done early on (before GIC code initializes)
-		 * because the hypervisor doesn't setup the GICR overlay pages
-		 * without this hypercall for VTL2. And without those GICR
-		 * addresses correctly setup, the GIC code will not correctly
-		 * initialize.
-		 */
-		status = hv_do_hypercall(HVCALL_ENABLE_VP_VTL, input, NULL);
-
-		if (!hv_result_success(status) &&
-		    hv_result(status) != HV_STATUS_VTL_ALREADY_ENABLED) {
-			if (hv_result(status) != HV_STATUS_INVALID_VP_INDEX) {
-				pr_err("HVCALL_ENABLE_VP_VTL failed for VP : %d ! [Err: %#llx\n]",
-					cpu, status);
-				ret = -EINVAL;
-			}
-			break;
-		}
-	}
-
-	local_irq_restore(irq_flags);
-
-	return ret;
+	return 0;
 }
 
 static int __init hv_vtl_cpu_prepare(unsigned int cpu)
@@ -74,7 +28,6 @@ static int hv_vtl_cpu_boot(unsigned int cpu)
 	unsigned long irq_flags;
 
 	local_irq_save(irq_flags);
-
 	input = *this_cpu_ptr(hyperv_pcpu_input_arg);
 	memset(input, 0, sizeof(*input));
 	input->partition_id = HV_PARTITION_ID_SELF;
@@ -83,14 +36,15 @@ static int hv_vtl_cpu_boot(unsigned int cpu)
 
 	/*
 	 * This is essentially all that is passed with the PSCI cpu_on
-	 * method, with x18 set to 0.
+	 * method, with x18 set to 0. The expectation is that the
+	 * `HVCALL_ENABLE_VP_VTL` has already been called at this point.
 	 */
 	input->vp_context.pc = (u64)__pa_symbol(secondary_entry);
 	status = hv_do_hypercall(HVCALL_START_VP, input, NULL);
 	if (!hv_result_success(status)) {
 		pr_err("HVCALL_START_VP failed for VP : %d ! [Err: %#llx]\n",
 		       cpu, status);
-		ret = -EINVAL;
+		ret = hv_status_to_errno(status);
 	}
 
 	local_irq_restore(irq_flags);
