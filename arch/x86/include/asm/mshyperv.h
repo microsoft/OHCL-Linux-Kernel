991c6b03820a575a077096acde16b72c501d79a8
Tianyu Lan <tiala@microsoft.com>
2025-02-12 11:59:22 +0530

    x86/sev: Use hvcall to convert apic id to vpid
    Call hv_vtl_apicid_to_vp_id() in the hv_snp_boot_ap()
    to convert apic id to vpid. The hvcall HVCALL_GET_VP_
    ID_FROM_APIC_ID will be used.
    
    Signed-off-by: Tianyu Lan <tiala@microsoft.com>

--- arch/x86/include/asm/mshyperv.h
+++ arch/x86/include/asm/mshyperv.h
@@ -303,6 +303,7 @@ static __always_inline u64 hv_raw_get_msr(unsigned int reg)
 	return __rdmsr(reg);
 }
 
+int hv_vtl_apicid_to_vp_id(u32 apic_id);
 #else /* CONFIG_HYPERV */
 static inline void hyperv_init(void) {}
 static inline void hyperv_setup_mmu_ops(void) {}
@@ -323,6 +324,7 @@ static inline void hv_set_msr(unsigned int reg, u64 value) { }
 static inline u64 hv_get_msr(unsigned int reg) { return 0; }
 static inline void hv_set_non_nested_msr(unsigned int reg, u64 value) { }
 static inline u64 hv_get_non_nested_msr(unsigned int reg) { return 0; }
+int hv_vtl_apicid_to_vp_id(u32 apic_id) { return -1; }
 #endif /* CONFIG_HYPERV */
 
 
