61ed5a5f02d56957501a479c14e62299d2dffd60
Roman Kisel <romank@linux.microsoft.com>
2025-04-28 11:27:05 -0700

    x86/hyperv: Fix APIC ID and VP index confusion in hv_snp_boot_ap()
    To start an application processor in SNP-isolated guest, a hypercall
    is used that takes a virtual processor index. The hv_snp_boot_ap()
    function uses that START_VP hypercall but passes as VP index to it
    what it receives as a wakeup_secondary_cpu_64 callback: the APIC ID.
    
    As those two aren't generally interchangeable, that may lead to hung
    APs if the VP index and the APIC ID don't match up.
    
    Update the parameter names to avoid confusion as to what the parameter
    is. Use the APIC ID to the VP index conversion to provide the correct
    input to the hypercall.
    
    Cc: stable@vger.kernel.org
    Fixes: 44676bb9d566 ("x86/hyperv: Add smp support for SEV-SNP guest")
    Signed-off-by: Roman Kisel <romank@linux.microsoft.com>
    Reviewed-by: Michael Kelley <mhklinux@outlook.com>

--- arch/x86/include/asm/mshyperv.h
+++ arch/x86/include/asm/mshyperv.h
@@ -304,7 +304,7 @@ static __always_inline u64 hv_raw_get_msr(unsigned int reg)
 	return __rdmsr(reg);
 }
 
-int hv_vtl_apicid_to_vp_id(u32 apic_id);
+int hv_apicid_to_vp_index(u32 apic_id);
 #else /* CONFIG_HYPERV */
 static inline void hyperv_init(void) {}
 static inline void hyperv_setup_mmu_ops(void) {}
@@ -325,7 +325,7 @@ static inline void hv_set_msr(unsigned int reg, u64 value) { }
 static inline u64 hv_get_msr(unsigned int reg) { return 0; }
 static inline void hv_set_non_nested_msr(unsigned int reg, u64 value) { }
 static inline u64 hv_get_non_nested_msr(unsigned int reg) { return 0; }
-int hv_vtl_apicid_to_vp_id(u32 apic_id) { return -1; }
+static inline int hv_apicid_to_vp_index(u32 apic_id) { return -EINVAL; }
 #endif /* CONFIG_HYPERV */
 
 
