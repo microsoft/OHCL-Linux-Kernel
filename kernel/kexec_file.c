8fb116e22954f6e46703d1e819e1b387dd3d70eb
Brian Mak <makb@juniper.net>
2025-08-05 14:15:27 -0700

    x86/kexec: carry forward the boot DTB on kexec
    Currently, the kexec_file_load syscall on x86 does not support passing a
    device tree blob to the new kernel.  Some embedded x86 systems use device
    trees.  On these systems, failing to pass a device tree to the new kernel
    causes a boot failure.
    
    To add support for this, we copy the behavior of ARM64 and PowerPC and
    copy the current boot's device tree blob for use in the new kernel.  We do
    this on x86 by passing the device tree blob as a setup_data entry in
    accordance with the x86 boot protocol.
    
    This behavior is gated behind the KEXEC_FILE_FORCE_DTB flag.
    
    Link: https://lkml.kernel.org/r/20250805211527.122367-3-makb@juniper.net
    Signed-off-by: Brian Mak <makb@juniper.net>
    Cc: Alexander Graf <graf@amazon.com>
    Cc: Baoquan He <bhe@redhat.com>
    Cc: Borislav Betkov <bp@alien8.de>
    Cc: Dave Young <dyoung@redhat.com>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Rob Herring <robh@kernel.org>
    Cc: Saravana Kannan <saravanak@google.com>
    Cc: Thomas Gleinxer <tglx@linutronix.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- kernel/kexec_file.c
+++ kernel/kexec_file.c
@@ -230,6 +230,9 @@ kimage_file_prepare_segments(struct kimage *image, int kernel_fd, int initrd_fd,
 		ret = 0;
 	}
 
+	image->no_cma = !!(flags & KEXEC_FILE_NO_CMA);
+	image->force_dtb = flags & KEXEC_FILE_FORCE_DTB;
+
 	if (cmdline_len) {
 		image->cmdline_buf = memdup_user(cmdline_ptr, cmdline_len);
 		if (IS_ERR(image->cmdline_buf)) {
