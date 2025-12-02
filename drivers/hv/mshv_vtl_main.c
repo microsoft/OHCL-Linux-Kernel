69a10166fe5df3cce87423e498d9e4f28b291805
Naman Jain <namjain@linux.microsoft.com>
2025-12-02 08:24:52 +0000

    copilation fail fixes
    Signed-off-by: Naman Jain <namjain@linux.microsoft.com>

--- drivers/hv/mshv_vtl_main.c
+++ drivers/hv/mshv_vtl_main.c
@@ -24,11 +24,14 @@
 #include <asm/pgalloc.h>
 #include <asm/mshyperv.h>
 #include <trace/events/ipi.h>
-#include <uapi/asm/mtrr.h>
 #include <uapi/linux/mshv.h>
 #include <hyperv/hvhdk.h>
 
+#ifdef CONFIG_X86_64
+#include <uapi/asm/mtrr.h>
+#include <asm/debugreg.h>
 #include "../../kernel/fpu/legacy.h"
+#endif
 #include "mshv.h"
 #include "mshv_vtl.h"
 #include "hyperv_vmbus.h"
