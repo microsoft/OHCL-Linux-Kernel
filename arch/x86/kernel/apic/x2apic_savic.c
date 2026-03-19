0955f991bc761df7396cefc0e47c4561d3f56778
Roman Kisel <romank@linux.microsoft.com>
2026-03-19 09:01:40 -0400

    drivers: hv: mshv_vtl: Support for Secure AVIC
    Signed-off-by: Roman Kisel <romank@linux.microsoft.com>
    Signed-off-by: Tianyu Lan <tiala@microsoft.com>

--- arch/x86/kernel/apic/x2apic_savic.c
+++ arch/x86/kernel/apic/x2apic_savic.c
@@ -331,6 +331,18 @@ static void savic_eoi(void)
 	}
 }
 
+void x2apic_savic_init_backing_page(void *ap)
+{
+	u32 apic_id;
+
+	/*
+	 * Before Secure AVIC is enabled, APIC msr reads are intercepted.
+	 * APIC_ID msr read returns the value from the Hypervisor.
+	 */
+	apic_id = native_apic_msr_read(APIC_ID);
+	apic_set_reg(ap, APIC_ID, apic_id);
+}
+
 static void savic_teardown(void)
 {
 	/* Disable Secure AVIC */
@@ -349,12 +361,7 @@ static void savic_setup(void)
 	if (!cc_platform_has(CC_ATTR_SNP_SECURE_AVIC))
 		return;
 
-	/*
-	 * Before Secure AVIC is enabled, APIC MSR reads are intercepted.
-	 * APIC_ID MSR read returns the value from the hypervisor.
-	 */
-	apic_set_reg(ap, APIC_ID, native_apic_msr_read(APIC_ID));
-
+	x2apic_savic_init_backing_page(ap);
 	gpa = __pa(ap);
 
 	gfn = gpa >> PAGE_SHIFT;
