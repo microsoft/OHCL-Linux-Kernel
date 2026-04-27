tiala/linux-sa

snp: return early in print_s5_reset_status_mmio() to avoid a triple fault
The new code introduced by commit ab8131028710 ("x86/CPU/AMD: Print the reason for the last reset")
exposes a bug in Hyper-V, and the ioread32() causes a triiple fault
on SNP. Let's work it around for now.

Signed-off-by: Dexuan Cui <decui@microsoft.com>

arch/x86/kernel/cpu/amd.c
@@ -24,6 +24,7 @@
 #include <asm/resctrl.h>
 #include <asm/msr.h>
 #include <asm/sev.h>
+#include <asm/mshyperv.h>
 
 #ifdef CONFIG_X86_64
 # include <asm/mmconfig.h>
@@ -1364,6 +1365,13 @@ static __init int print_s5_reset_status_mmio(void)
 	void __iomem *addr;
 	u32 value;
 	int i;
+
+	/*
+	 * The below ioread32() causes a triple fault for SNP due to a hypevisor
+	 * bug. Work it around for now.
+	 */
+	if (!ms_hyperv.paravisor_present && hv_isolation_type_snp())
+		return 0;
 
 	if (!cpu_feature_enabled(X86_FEATURE_ZEN))
 		return 0;

microsoft/OHCL-Linux-Kernel

arch/x86/kernel/cpu/amd.c
@@ -24,6 +24,7 @@
 #include <asm/resctrl.h>
 #include <asm/msr.h>
 #include <asm/sev.h>
+#include <asm/mshyperv.h>
 
 #ifdef CONFIG_X86_64
 # include <asm/mmconfig.h>
@@ -1364,6 +1365,13 @@ static __init int print_s5_reset_status_mmio(void)
 	void __iomem *addr;
 	u32 value;
 	int i;
+
+	/*
+	 * The below ioread32() causes a triple fault for SNP due to a hypevisor
+	 * bug. Work it around for now.
+	 */
+	if (!ms_hyperv.paravisor_present && hv_is_isolation_supported())
+		return 0;
 
 	if (!cpu_feature_enabled(X86_FEATURE_ZEN))
 		return 0;